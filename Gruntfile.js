ac7c3ba5785f92f336f5d3587e1132a60a0f57c2
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-02-26 15:32:10 -0600

    another gruntfile tweak

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
             "options": {
                 "configFile": "eslintrc.json"
             },
-            "target": ["src/*.js", "spec/*.js"]
+            "target": ["src/*.js", "tests/*.js"]
         },
         "githooks": {
             "all": {
